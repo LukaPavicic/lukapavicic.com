2b5b3f82b98ae0cf0837c1007cbf13a22c284613
Luka Pavicic <dev.luka.pavicic@gmail.com>
2020-05-24 10:11:05 +0200

    fixed aboutme text

--- src/components/Home/AboutMe.jsx
+++ src/components/Home/AboutMe.jsx
@@ -9,8 +9,8 @@ export default function AboutMe(props) {
             <Typography variant="h6">
                 I'm an 18-year-old developer from Zagreb, Croatia. My favorite technology is Ruby on Rails and  I love working 
                 with everything Ruby related. I'm trying to 
-                learn something new every day whether it is Ruby related or not. Sometimes in my free time I like to spend some time 
-                on StackOverflow answering questions or learning from them. My other non-programming related hobbies are mountain biking, football 
+                learn something new every day whether it is Ruby related or not. I spend some of my free time answering questions on StackOverflow 
+                or learning from them. My other non-programming related hobbies are mountain biking, football 
                 or just doing anything fun.
             </Typography>
         </div>
